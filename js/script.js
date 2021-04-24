8745664570e7ed0c0a0e38085c408af7214fa62e
Tawmy <mail@tawmy.dev>
2021-04-24 13:36:48 +0200

    Initial Commit
    .gitignore, project structure, first draft of menu

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(function () {
+
+    $('#hamburger-button').on("click", function () {
+
+        let i = 0;
+        $("#nav-links").children("a").each(function () {
+            $(this).css({"animation-delay": i * 50 + "ms"});
+            i++;
+        });
+
+        $(this).parent("header").parent("nav").toggleClass("nav-visible");
+    });
+
+
+});
